cbc9fa77c328611875d72e2fd6b9dfe490d62d31
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-11 15:46:34 +0100

    Appheader

--- react-task-list/src/components/AppHeader.jsx
+++ react-task-list/src/components/AppHeader.jsx
@@ -1,2 +1,22 @@
 // usa dei paths relativi per accedere ai file dentro (src folder)
 import reactLogo from '../assets/react.svg'
+// usa paths assoluto per accedere ai file dentro (public folder)
+import viteLogo from '/vite.svg'
+
+export default function AppHeader() {
+
+    // Metti qui la logica
+    const title = 'JSX Task List';
+
+    // Metti qui il markup
+    return (
+        <header>
+
+            <div className="logo">
+                <img src={reactLogo} alt="" />
+                {title.toUpperCase()}
+            </div>
+
+        </header>
+    )
+}
